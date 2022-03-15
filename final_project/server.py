bac08e1ad022d4fce9c2b4cccbd974c24e811a2d
SergeWambo <tekafowambo@yahoo.fr>
2022-03-15 23:45:17 +0000

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,36 @@
-from machinetranslation import translator
+'''server module : renders an iindex file on port 8080
+   allow traduction of text from input text to either english or french
+'''
 from flask import Flask, render_template, request
-import json
+from machinetranslation import translator
+
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+def english_to_french():
+    '''
+        english to french function route
+    '''
+    text_to_translate = request.args.get('textToTranslate')
+    translated_text = translator.english_to_french(text_to_translate)
+    return translated_text
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+def french_to_english():
+    '''
+        french to english function route
+    '''
+    text_to_translate = request.args.get('textToTranslate')
+    translated_text = translator.french_to_english(text_to_translate)
+    return translated_text
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+def render_index_page():
+    '''
+        index rendering function route
+    '''
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
